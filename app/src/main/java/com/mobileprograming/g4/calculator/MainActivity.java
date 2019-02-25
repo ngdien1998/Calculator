7d742d8942aeaae35f17cba3593f17d93c776c3e
Nguyen Le Dien <ngdien1998@gmail.com>
2019-02-25 13:40:52 +0700

    Nguyen Le Dien commit

---
+++ app/src/main/java/com/mobileprograming/g4/calculator/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mobileprograming.g4.calculator;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mobileprograming.g4.calculator.fragments.CalculatorFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new CalculatorFragment());
+        fragmentTransaction.commit();
+    }
+}
